8aad03fcf0ae6dba4e71d49bd8947d3a2a11ac71
Eurymachus <reflex_ion@hotmail.com>
2013-09-21 21:18:43 +0100

    Updated Classpath

--- MTL-source/eurymachus/mtl/core/MultiTexturedLevers.java
+++ MTL-source/eurymachus/mtl/core/MultiTexturedLevers.java
@@ -2,7 +2,9 @@ package eurymachus.mtl.core;
 
 import slimevoidlib.ICommonProxy;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -26,17 +28,17 @@ public class MultiTexturedLevers {
 	@SidedProxy(
 			clientSide = CoreLib.CLIENT_PROXY,
 			serverSide = CoreLib.COMMON_PROXY)
-	public static ICommonProxy proxy;
+	public static ICommonProxy	proxy;
 
-	@EventHandler
+	@PreInit
 	public void MultiTexturedLeversPreInit(FMLPreInitializationEvent event) {
 	}
 
-	@EventHandler
+	@Init
 	public void MultiTexturedLeversInit(FMLInitializationEvent event) {
 	}
 
-	@EventHandler
+	@PostInit
 	public void MultiTexturedLeversPostInit(FMLPostInitializationEvent event) {
 		MTLInit.initialize();
 	}
